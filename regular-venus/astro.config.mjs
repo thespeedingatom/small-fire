b867f2c1abf9e5a981c04dc568bc2ed694e5c397
thespeedingatom <thespeedingatom@gamil.com>
2025-03-04 11:30:17 +0800

    Add cloudflare and update dependencies

--- regular-venus/astro.config.mjs
+++ regular-venus/astro.config.mjs
@@ -5,16 +5,22 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), icon(), sitemap()],
   site: 'https://launchsphere.com',
   base: '/',
   trailingSlash: 'never',
+
   build: {
     format: 'file'
   },
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  output: 'server',
+  adapter: cloudflare()
 });
